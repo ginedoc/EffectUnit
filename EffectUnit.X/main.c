b112912b0d2b2a4ddd83d27e18e3e07056bb9ae2
ginedoc <bear110088@gmail.com>
2018-12-12 15:26:19 +0800

    1

--- EffectUnit.X/main.c
+++ EffectUnit.X/main.c
@@ -7,7 +7,18 @@
 
 
 #include <xc.h>
+#include "effect.h"
 
 void main(void) {
+    setting_sound_input();
     return;
+}
+void setting_sound_input(){
+    // ADC pin
+    
+}
+
+
+void interrupt HighISR(){
+    
 }
